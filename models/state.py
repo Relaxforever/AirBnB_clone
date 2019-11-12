65e469246188e343bd5ba383150f517d9d95ddf7
jdarangop <juandiegoap33@gmail.com>
2019-11-12 19:27:48 +0000

    Add: state subclass of BaseModel

---
+++ models/state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Subclass of BaseModel in charge of saving the state name """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class Used to represent an American State
+        Attributes
+        ----------
+        name : str
+            the name of the state
+            """
+
+    def __init__(self, name="", *args, **kwargs):
+        """ Parameters
+            ----------
+            name : str
+                the name of the state
+                """
+        super().__init__(*args, **kwargs)
