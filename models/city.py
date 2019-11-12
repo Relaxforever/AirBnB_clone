655133651d2c38ad3f2eabb0786b03c7d4ebdf7f
Fidel Caicedo <869@holbertonschool.com>
2019-11-12 19:27:35 +0000

    uploading cities

---
+++ models/city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Subclass of BaseModel in charge of saving the city name """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Class Used to represent an American City
+        Attributes
+        ----------
+        state_id: str
+            it will be the id of the state
+        name : str
+            the name of the city
+            """
+
+    def __init__(self, state_id="", name="", *args, **kwargs):
+        """ Parameters
+            ----------
+            state_id : str
+                The id of the state
+            name : str
+                the name of the state
+                """
+        super().__init__(*args, **kwargs)
