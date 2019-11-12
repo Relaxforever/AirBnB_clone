f529f67b8e6becf1a0183fbd93b455789a0baaad
Fidel Caicedo <869@holbertonschool.com>
2019-11-12 19:16:53 +0000

    added: Amenity

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Subclass of BaseModel in charge of saving the amenity name """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Used to represent an Amenity
+        Attributes
+        ----------
+        name : str
+            the name of the amenity
+            """
+
+    def __init__(self, name="", *args, **kwargs):
+        """ Parameters
+            ----------
+            name : str
+                the name of the amenity
+                """
+        super().__init__(*args, **kwargs)
